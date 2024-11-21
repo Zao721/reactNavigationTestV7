ab211bc1ae991a691cd234dba29fa41c273f5e90
Dmitrii Lazarev <dmitrii.lazarev@stranadev.ru>
2024-11-21 08:25:20 +0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -5,114 +5,132 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { NavigationContainer, createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    Text, View
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function ScreenA() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen A</Text>
+    </View>
+  );
+}
+function ScreenB() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen B</Text>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+//#region StaticAPI
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const ConditionContext = createContext(0)
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+const ConditionContextProvider = ({ children } : PropsWithChildren) => {
+    const [number, setNumber] = useState(0)
+    useEffect(() => {
+        const interval = setInterval(
+            () => {
+                setNumber(_number => {
+                    console.log(_number + 1)
+                    return _number + 1
+                })
+            },
+            2000
+        )
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    return (
+        <ConditionContext.Provider value={number}>
+            {children}
+        </ConditionContext.Provider>
+    )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+const useCheckConditionA = () => {
+    const value = useContext(ConditionContext)
+
+    return value < 5
+}
+
+const useCheckConditionB = () => {
+    const value = useContext(ConditionContext)
+
+    return value >= 5
+}
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    ScreenA: {
+      if: useCheckConditionA,
+      screen: ScreenA,
+    },
+    ScreenB: {
+      if: useCheckConditionB,
+      screen: ScreenB,
+    },
   },
 });
+
+const Navigation = createStaticNavigation(RootStack);
+
+const App = () => {
+    return (
+        <ConditionContextProvider>
+            <Navigation />
+        </ConditionContextProvider>
+    );
+}
+//#endregion
+
+//#region DynamicAPI
+
+//  const Stack = createNativeStackNavigator();
+
+// function RootStack() {
+//     const [number, setNumber] = useState(0)
+
+//     useEffect(() => {
+//         const interval = setInterval(
+//             () => {
+//                 setNumber(_number => {
+//                     console.log(_number + 1)
+//                     return _number + 1
+//                 })
+//             },
+//             2000
+//         )
+//         return () => {
+//             clearInterval(interval)
+//         }
+//     }, [])
+
+//     return (
+//         <Stack.Navigator>
+//             {(number > 5) ? (
+//                 <Stack.Screen name="ScreenA" component={ScreenA} />
+//             ) : (<Stack.Screen name="ScreenB" component={ScreenB} />
+//             )}
+//         </Stack.Navigator>
+//     );
+// }
+
+// const App = () => {
+//     return (
+//         <NavigationContainer>
+//             <RootStack />
+//         </NavigationContainer>
+//     );
+// }
+
+//#endregion
 
 export default App;
